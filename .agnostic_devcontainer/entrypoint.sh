64751b14a7a9918be5da6de34ba9b5e05d20ebae
Juan Pineda <juan.david.pineda@gmail.com>
2023-04-28 13:42:08 -0500

    fix order

--- .agnostic_devcontainer/entrypoint.sh
+++ .agnostic_devcontainer/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Fix TZ
+sudo ln --symbolic --force /usr/share/zoneinfo/"$(curl http://ip-api.com/json/ \
+--silent | jq .timezone | tr -d '"')" /etc/localtime
+
 # Install pre-commit
-if [ -d "/${WORKDIR}/${PROJ_NAME}.git" ] && [ -f "/workspaces/.pre-commit-config.yaml" ]; then
+if [ -d "/${WORKDIR}/${PROJ_NAME}/.git" ] && [ -f "/${WORKDIR}/${PROJ_NAME}/.pre-commit-config.yaml" ]; then
     echo "Installing pre-commit"
     pre-commit install
     pre-commit install-hooks
@@ -9,6 +13,4 @@ else
     echo "Not installing pre-commit"
 fi
 
-# Fix TZ
-sudo ln --symbolic --force /usr/share/zoneinfo/"$(curl http://ip-api.com/json/ --silent | jq .timezone | tr -d '"')" /etc/localtime
 exec "$@"
